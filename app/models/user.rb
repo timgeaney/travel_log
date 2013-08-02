d4670bc09115a962532f097eee7766a54876c18c
tim geaney <timgeaney24@gmail.com>
2013-08-02 16:46:07 +0100

    added home page

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,11 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  # attr_accessible :title, :body
+
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+
+  validates_presence_of :name
+  validates_uniqueness_of :name, :email, :case_sensitive => false
+
+
 end
