1609ee4b2a0bdfc778f2bf2e2f729361257addbe
tim geaney <timgeaney24@gmail.com>
2013-08-02 15:56:05 +0100

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TravelLog::Application.config.secret_token = '64d75f89f9d5fe92d638ca8b2595bfa03719064039d4327b90091392c5c06e78c30502eb87c7a2d8265a2767f8e2b7b136a415791497aac5b002e224b2c3e571'
